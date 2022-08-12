fae187959d457cf0ea12aad370afb75e03c4aeb8
caixiangfan <caixiang@ualberta.ca>
2022-08-12 15:36:04 +0000

    front pages design

--- pages/admin.js
+++ pages/admin.js
@@ -1,57 +1,63 @@
 import React, { useContext, useState, useEffect } from 'react'
 // import axios from 'axios';
-// import { Typography, Box, Paper, Button, TextField, InputLabel, MenuItem, FormControl, Select, CircularProgress } from '@mui/material';
+import { Typography, Box, Grid, Button, ListItem, List, TextField, InputLabel, MenuItem, FormControl, Select, Paper } from '@mui/material';
+// import List from '@mui/joy/List';
+// import ListItem from '@mui/joy/ListItem';
 // import { useRouter } from 'next/router'
 // import { useSnackbar } from 'notistack';
 import Layout from 'components/layout'
-// import { Store } from "utils/Store";
-// import { ethers } from 'ethers';
-// import { contractAddress, defaultProvider, contractRead, backendUrl } from "utils/const"
-// import abi from 'utils/contracts/abi.json'
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
 
 const Admin = () => {
-  // const router = useRouter()
-  // const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-  // const { state, dispatch } = useContext(Store);
-  // const { adminAddress, walletConencted, correctNetworkConnected, account, provider, signer, ticketCategories } = state;
+  const [usertype, setUsertype] = React.useState('');
 
-  // const [loaded, setLoaded] = useState(false);
-  // const [adminConnected, setAdminConnected] = useState(false);
-  // // SET UP NEW TICKET
-  // const [newName, setNewName] = useState('')
-  // const [newTicketPrice, setNetTicketPrice] = useState('')
-  // const [newMaxNumOfTicket, setNewMaxNumOfTicket] = useState(0)
-
-  // const onNameChangeHandler = (e) => {
-  //   setNewName(e.target.value)
-  // }
-
-  // const onTicketPriceChangeHandler = (e) => {
-  //   setNetTicketPrice(e.target.value)
-  // }
-
-  // const onMaxNumTicketChangeHandler = (e) => {
-  //   setNewMaxNumOfTicket(e.target.value)
-  // }
-
-  // const onCreateTicketHandler = (e) => {
-  //   const create = async () => {
-  //     try {
-  //       const _name = ethers.utils.formatBytes32String(newName)
-  //       const _price = ethers.utils.parseEther(newTicketPrice)
-  //       const _maxNumber = parseInt(newMaxNumOfTicket)
-  //       const contractWrite = new ethers.Contract(contractAddress, abi, signer)
-  //       await contractWrite.setUpTicket(_name, _price, _maxNumber, 0)
-  //       enqueueSnackbar("Ticket Creation Submitted", { variant: "info" })
-  //     }
-  //     catch (error) {
-  //       enqueueSnackbar("Create Ticket Error:", error)
-  //     }
-  //   }
-  //   create()
-  // }
-
-
+  const handleChange = (event) => {
+    setUsertype(event.target.value);
+  };
   // useEffect(() => {
   //   if (adminAddress.length === 0 || account.length == 0) {
   //     setLoaded(false)
@@ -70,7 +76,74 @@ const Admin = () => {
 
   return (
     <Layout title="Admin">
-      <h1>This is Admin page!</h1>
+      <Box sx = {{
+        alignItems: 'center',
+        marginTop: '2rem',
+      }}
+      >
+        <Typography item component="h1" variant="h5" align='center'>
+          Search Participants from Blockchain
+        </Typography>
+      </Box>
+      
+      <Box
+        sx={{
+          marginTop: '3rem',
+          display: 'flex',
+          // gridTemplateRows: 'repeat(2, 1fr)',
+          alignItems: 'center',
+          justifyContent: 'center',
+          '& > :not(style)': {
+            m: 1,
+            maxWidth: '80vw',
+            maxHeight: '80vh',
+            height: 600,
+            width: 700,
+          },
+        }}
+      >
+
+        <Grid container spacing={2}>
+
+          <Grid item xs={12} sm={9}>
+            <FormControl fullWidth size='small'>
+              <InputLabel id="demo-simple-select-label">User Type</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={usertype}
+                label="User Type"
+                onChange={handleChange}
+              >          
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>All Registered Suppliers</MenuItem>
+                <MenuItem value={20}>All Registered Consumers</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12} sm={3}>
+            <Button variant="Search">Search</Button>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Box sx={{ height: 500, width: '100%' }}>
+              <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={10}
+                rowsPerPageOptions={[10]}
+                checkboxSelection
+                disableSelectionOnClick
+                experimentalFeatures={{ newEditingApi: true }}
+              />
+            </Box>
+          </Grid>
+
+        </Grid>
+      </Box>
     </Layout>
   )
 }
--- pages/buy-etk.js
+++ pages/buy-etk.js
@@ -1,9 +1,159 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Layout from 'components/layout';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {
+    Avatar,
+    Button,
+    Select,
+    MenuItem,
+    FormControl,
+    FormControlLabel,
+    InputLabel,
+    TextField,
+    CssBaseline,
+    Container,
+    Typography,
+    Checkbox,
+    Link,
+    Grid,
+    Box
+  } from '@mui/material';
+
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const BuyETK = () => {
+
+  const [usertype, setUsertype] = React.useState('');
+
+  const handleChange = (event) => {
+    setUsertype(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
   return <Layout title='BuyETK'>
-    <h1>This is BuyETK page!</h1>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <MonetizationOnIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Buy Energy Token (ETK)
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+
+              <Grid item xs={12} sm={12}>
+                <FormControl fullWidth required size='small'>
+                  <InputLabel id="demo-simple-select-label">User Type</InputLabel>
+                  <Select
+                    labelId="user-type-select"
+                    id="user-type-select"
+                    value={usertype}
+                    label="User Type"
+                    onChange={handleChange}
+                  >          
+                    <MenuItem value={10}>Supplier</MenuItem>
+                    <MenuItem value={20}>Consumer</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  autoComplete="AssetID"
+                  name="assetID"
+                  required
+                  fullWidth
+                  id="assetID"
+                  label="AssetID"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="capacityOrLoad"
+                  label="Capacity or Load"
+                  name="capacityOrLoad"
+                  autoComplete="capacityOrLoad"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="blockAmount"
+                  label="Block Amount"
+                  name="blockAmount"
+                  autoComplete="blockAmount"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  name="orderControl"
+                  label="Order Control (Optional)"
+                  type="orderControl"
+                  id="orderControl"
+                  autoComplete="orderControl"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Submit
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
   </Layout>
 }
 
