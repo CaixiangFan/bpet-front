da90b4fd83fefeebda14e7f1472e9c17b3671c88
caixiangfan <caixiang@ualberta.ca>
2022-08-30 15:54:06 +0000

    finish submit bid

--- pages/submit-bid.js
+++ pages/submit-bid.js
@@ -1,7 +1,16 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Layout from 'components/layout';
+import { ethers } from "ethers";
 import ElectricalServicesIcon from '@mui/icons-material/ElectricalServices';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {
+  registryContractRead, 
+  REGISTRY_CONTRACT_ADDRESS,
+  poolmarketContractRead,
+  POOLMARKET_CONTRACT_ADDRESS
+} from 'utils/const';
+import poolmarketAbi from 'utils/contracts/PoolMarket.sol/PoolMarket.json'
+import { useSnackbar, closeSnackbar } from 'notistack';
+import { Store } from "utils/Store";
 import {
     Avatar,
     Button,
@@ -23,20 +32,96 @@ import {
 
 const SubmitBid = () => {
 
-  const [usertype, setUsertype] = React.useState('');
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const { state, dispatch } = useContext(Store);
+  const { walletConencted, correctNetworkConnected, account, provider, signer } = state;
+  const [usertype, setUsertype] = useState('');
+  const [assetId, setAssetId] = useState('');
+  const [load, setLoad] = useState(0);
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(0);
+  const [isRegisteredConsumer, setIsRegisteredConsumer] = useState(false);
 
-  const handleChange = (event) => {
-    setUsertype(event.target.value);
+  useEffect(() => {
+    if (account.length === 0) return
+    const checkRegistered = async () => {
+      const _isRegisteredConsumer = await registryContractRead.isRegisteredConsumer(account);
+      if (_isRegisteredConsumer) {
+        const registeredConsumer = await registryContractRead.getConsumer(account);
+        const _minPrice = await poolmarketContractRead.minAllowedPrice();
+        const _maxPrice = await poolmarketContractRead.maxAllowedPrice();
+        setAssetId(registeredConsumer.assetId);
+        // setBlockAmount(registeredConsumer.blockAmount);
+        setLoad(registeredConsumer.load);
+        setMinPrice(_minPrice);
+        setMaxPrice(_maxPrice);
+      } else {
+        enqueueSnackbar('This account has not been registered as consumer!', { variant: 'info', preventDuplicate: true});
+      }
+      setIsRegisteredConsumer(_isRegisteredConsumer);
+    }
+    checkRegistered();
+  }, [account])
+
+  const inputValidate = (bidData) => {
+    let allValid = true;
+    //check empty inputs
+    if (bidData.energyAmount.length === 0) {
+      allValid = false;
+      enqueueSnackbar('You must input an amount', { variant: 'error' })
+    }
+
+    if (bidData.price.length === 0) {
+      allValid = false;
+      enqueueSnackbar('You must input a price', { variant: 'error' })
+    }
+
+    if (Number(bidData.energyAmount) <= 0 || Number(bidData.energyAmount) > load) {
+      allValid = false;
+      enqueueSnackbar('You must input a positive amount within load', { variant: 'error' })
+    }
+
+    if (Number(bidData.price) < minPrice || Number(bidData.price) > maxPrice) {
+      allValid = false;
+      enqueueSnackbar(`You must input a price between ${minPrice} and ${maxPrice}`, { variant: 'error' })
+    }
+    return allValid;
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    const bidData = {
+      assetID: assetId,
+      energyAmount: data.get('amount'),
+      price: data.get('price')
+    };
+    const valid = inputValidate(bidData);
+    if (!valid) return;
+
+    if (!walletConencted || !signer) {
+      enqueueSnackbar("You must login Metamask to continue", { variant: 'info', preventDuplicate: true});
+      return
+    }
+    const submitABid = async (bidData) => {
+      await submitBid(bidData);
+      enqueueSnackbar("You submitted an offer successfully!", { variant: 'success', preventDuplicate: true});
+    }
+    submitABid(bidData);
   };
+
+  const submitBid = async (data) => {
+    try {
+      const poolmarketContractWrite = new ethers.Contract(POOLMARKET_CONTRACT_ADDRESS, poolmarketAbi, signer);
+      await poolmarketContractWrite.submitBid(
+        data.assetID,
+        Number(data.energyAmount),
+        Number(data.price)
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return <Layout title='SubmitBid'>
       <Container component="main" maxWidth="xs">
@@ -57,23 +142,6 @@ const SubmitBid = () => {
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-
-              <Grid item xs={12} sm={12}>
-                <FormControl fullWidth required size='small'>
-                  <InputLabel id="demo-simple-select-label">User Type</InputLabel>
-                  <Select
-                    labelId="user-type-select"
-                    id="user-type-select"
-                    value={usertype}
-                    label="User Type"
-                    onChange={handleChange}
-                  >          
-                    <MenuItem value={10}>Supplier</MenuItem>
-                    <MenuItem value={20}>Consumer</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
-
               <Grid item xs={12} sm={12}>
                 <TextField
                   autoComplete="AssetID"
@@ -81,38 +149,29 @@ const SubmitBid = () => {
                   required
                   fullWidth
                   id="assetID"
-                  label="AssetID"
-                  autoFocus
+                  label={isRegisteredConsumer ? assetId : 'AssetID'}
+                  disabled={isRegisteredConsumer}
                 />
               </Grid>
               <Grid item xs={12} sm={12}>
                 <TextField
                   required
                   fullWidth
-                  id="capacityOrLoad"
-                  label="Capacity or Load"
-                  name="capacityOrLoad"
-                  autoComplete="capacityOrLoad"
+                  autoFocus
+                  id="amount"
+                  label={isRegisteredConsumer ? `Amount 0 ~ ${load}` : 'Amount'}
+                  name="amount"
+                  autoComplete="amount"
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
-                  id="blockAmount"
-                  label="Block Amount"
-                  name="blockAmount"
-                  autoComplete="blockAmount"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  name="orderControl"
-                  label="Order Control (Optional)"
-                  type="orderControl"
-                  id="orderControl"
-                  autoComplete="orderControl"
+                  id="price"
+                  label={isRegisteredConsumer ? `Price ${minPrice} ~ ${maxPrice}` : 'Price'}
+                  name="price"
+                  autoComplete="price"
                 />
               </Grid>
               <Grid item xs={12}>
@@ -127,6 +186,7 @@ const SubmitBid = () => {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              disabled={!isRegisteredConsumer}
             >
               Submit
             </Button>
